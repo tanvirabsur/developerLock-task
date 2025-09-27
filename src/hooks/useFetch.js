95ee710bfce2f2bd7c4f51b29761f1202e540087
tanvirabsar <tjoy4799@gmail.com>
2025-09-27 21:03:05 +0600

    add basic setup

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const useFetch = (url) => {
-    return url
+    const [data, setData] = useState()
+
+    useEffect(() => {
+        fetch(url)
+            .then(res => res.json())
+            .then(res => setData(res))
+    }, [url])
+
+    
+    return data
 };
 
 export default useFetch;
